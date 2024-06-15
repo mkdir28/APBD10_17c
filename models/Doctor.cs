f1db2b48e31075b91b435305cb78ae302f26b3c3
Maryna Kamienieva <mkdirwd@gmail.com>
2024-06-15 13:40:39 +0200

    added model Doctor.cs

---
+++ models/Doctor.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeFist.models;
+
+[Table("Doctor")]
+public class Doctor
+{
+    [Key]
+    public int IdDoctor { get; set; }
+    [MaxLength(100)]
+    public string FiestName { get; set; } = string.Empty;
+    [MaxLength(100)]
+    public string LastName { get; set; } = string.Empty;
+    [MaxLength(100)]
+    public string Email { get; set; } = string.Empty;
+    
+    public ICollection<Prescription> Prescriptions { get; set; } = new HashSet<Prescription>();
+}
