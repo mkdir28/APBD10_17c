d1b24197201cdcb9badcb94e515b82980d75704b
Maryna Kamienieva <mkdirwd@gmail.com>
2024-06-20 09:36:44 +0200

    added Medicament.cs model

---
+++ models/Medicament.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using DefaultNamespace;
+
+
+namespace CodeFist.models;
+
+[Table("Medicament")]
+public class Medicament
+{
+    [Key]
+    public int IdMedicament { get; set; }
+    [MaxLength(100)]
+    public string Name { get; set; } = string.Empty;
+    [MaxLength(100)]
+    public string Description { get; set; } = string.Empty;
+    [MaxLength(100)]
+    public string Type { get; set; } = string.Empty;
+    
+    public ICollection<Prescription_Medicament> Prescription_Medicaments { get; set; } = new HashSet<Prescription_Medicament>();
+}
