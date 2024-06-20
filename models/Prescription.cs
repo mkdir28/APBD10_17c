fc46dcb2601628f7d50e6092e1c178a87c67dd52
Maryna Kamienieva <mkdirwd@gmail.com>
2024-06-20 11:50:49 +0200

    updated model

--- models/Prescription.cs
+++ models/Prescription.cs
@@ -6,13 +6,15 @@ using Microsoft.EntityFrameworkCore;
 namespace CodeFirst.models;
 
 [Table("Prescription")]
-[PrimaryKey(nameof(IdPatient), nameof(IdDoctor))]
 public class Prescription
 {
     [Key]
     public int IdPrescription { get; set; }
     public DateTime Date { get; set; }
     public DateTime? DueDate { get; set; }
+    public int IdPatient { get; set; }
+    public int IdDoctor { get; set; }
+
     
     [ForeignKey(nameof(IdPatient))]
     public Patient Patient { get; set; } = null!;
