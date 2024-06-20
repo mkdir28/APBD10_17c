c4721ff760a5c0a01f193a9d5ae40b6e4156d409
Maryna Kamienieva <mkdirwd@gmail.com>
2024-06-20 20:36:34 +0200

    updated dto

--- dto's/AddPrescriptionDTO.cs
+++ dto's/AddPrescriptionDTO.cs
@@ -3,11 +3,44 @@ using CodeFist.models;
 
 namespace APBD10_17c.dyp_s;
 
-public class AddPrescriptionDTO
+public class AddPatientDTO
 {
-    public Patient Patient { set; get; } = null;
-    public Prescription Prescription { set; get; }
-    public Doctor Doctor { set; get; } = null;
+    public PatientDTO PatientDto { get; set; } = null;
+    public MedicamentsDTO MedicamentsDTO { get; set; } = null;
+    public DateTime Date { get; set; }
+    public DateTime? DueDate { get; set; }
+    
+    public ICollection<MedicamentsDTO> Medicaments = new List<MedicamentsDTO>();
+}
 
-    public ICollection<Medicament> Medicaments = new List<Medicament>();
+public class PatientDTO
+{
+    public int IdPatient { get; set; }
+    public string FiestName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+}
+
+public class PresiptionDTO
+{
+    public int IdPrescription { get; set; }
+    public DateTime Date { get; set; }
+    public DateTime? DueDate { get; set; }
+    
+    public ICollection<DoctorDTO> Doctors = new List<DoctorDTO>();
+    public ICollection<MedicamentsDTO> Medicaments = new List<MedicamentsDTO>();
+}
+
+public class DoctorDTO
+{
+    public int IdDoctor { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+}
+
+public class MedicamentsDTO
+{
+    public int IdMedicament { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Dose { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
 }
