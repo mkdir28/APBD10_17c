40b09a35e843b83ec9a704256b7b03fdd754d8b6
Maryna Kamienieva <mkdirwd@gmail.com>
2024-06-20 10:33:29 +0200

    added Prescription_Medicament.cs model

---
+++ models/Prescription_Medicament.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using CodeFist.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirst.models;
+
+[Table("Prescription_Medicament")]
+[PrimaryKey(nameof(IdMedicament), nameof(IdPrescription))]
+public class Prescription_Medicament
+{
+    public int? Dose { get; set; }
+    [MaxLength(100)]
+    public string Details { get; set; }
+    
+    [ForeignKey(nameof(IdMedicament))]
+    public Medicament Medicament { get; set; } = null!;
+    [ForeignKey(nameof(IdPrescription))]
+    public Prescription Prescription { get; set; } = null!;
+    
+    public ICollection<Prescription_Medicament> PrescriptionMedicaments { get; set; } = new HashSet<Prescription_Medicament>();
+}
