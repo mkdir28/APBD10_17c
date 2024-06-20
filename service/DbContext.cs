171cf6d9802c2fe1589faa30de3c68833b7d77b2
Maryna Kamienieva <mkdirwd@gmail.com>
2024-06-20 20:43:34 +0200

    added business logic

--- service/DbContext.cs
+++ service/DbContext.cs
@@ -1,6 +1,35 @@
 namespace APBD10_1_17c.services;
 
-public class DbContext
+
+public class DbContext: IDbContext
 {
+    private readonly DatabaseConnect _context;
+    public DbContext(DatabaseConnect context)
+    {
+        _context = context;
+    }
     
-}
+    public async Task<AddPrescriptionDTO?> GetPatientinformation(int id)
+    {
+        return await _context.Patients
+            .Include(e => e.IdPatient)
+            .ThenInclude(e => e.Prescription_Medicaments)
+            .Select()
+            .ToListAsync();
+    }
+
+    public async Task<bool> DoesPatientExist(int id)
+    {
+        return await _context.Patients.AnyAsync(e => e.IdPatient == id);
+    }
+
+    public async Task AddPatient(Patient patient)
+    {
+        await _context.AddAsync(patient);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<bool> DoesMedicamentsExist(int id)
+    {
+        return await _context.Medicaments.AnyAsync(e => e.IdMedicament == id);
+    }
--- service/IDbContext.cs
+++ service/IDbContext.cs
@@ -1,6 +1,10 @@
 namespace APBD10_1_17c.services;
-
+using APBD10_17c.dto_s;
+using CodeFist.models;
 public interface DbContext
 {
-    
+    Task<AddPrescriptionDTO?> GetPatientinformation(int id);
+    Task<bool> DoesPatientExist(int id);
+    Task AddPatient(Patient patient);
+    Task<bool> DoesMedicamentsExist(int id);
 }
