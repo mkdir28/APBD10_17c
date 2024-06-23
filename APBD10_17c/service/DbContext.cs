8d0c506d2084f81decda3a1e4407def645185da8
Maryna Kamienieva <mkdirwd@gmail.com>
2024-06-23 09:35:25 +0200

    updated DbContext.cs

--- APBD10_17c/service/DbContext.cs
+++ APBD10_17c/service/DbContext.cs
@@ -12,10 +12,32 @@ public class DbContext: IDbContext
     public async Task<AddPrescriptionDTO?> GetPatientinformation(int id)
     {
         return await _context.Patients
-            .Include(e => e.IdPatient)
-            .ThenInclude(e => e.Prescription_Medicaments)
-            .Select()
-            .ToListAsync();
+            .Include(p => p.Prescriptions)
+            .ThenInclude(pr => pr.PrescriptionMedicaments)
+            .ThenInclude(pm => pm.Medicament)
+            .Where(p => p.IdPatient == id)
+            .Select(p => new AddPrescriptionDTO
+            {
+                Patient = new PatientDTO
+                {
+                    IdPatient = p.IdPatient,
+                    FiestName = p.FiestName,
+                    LastName = p.LastName,
+                    Date = p.Date
+                },
+                Medicaments = p.Prescriptions
+                    .SelectMany(pr => pr.PrescriptionMedicaments)
+                    .Select(pm => new MedicamentsDTO
+                    {
+                        IdMedicament = pm.Medicament.IdMedicament,
+                        Name = pm.Medicament.Name,
+                        Dose = pm.Dose.ToString(),
+                        Description = pm.Description
+                    }).ToList(),
+                Date = p.Prescriptions.FirstOrDefault().Date,
+                DueDate = p.Prescriptions.FirstOrDefault().DueDate
+            })
+            .FirstOrDefaultAsync();
     }
 
     public async Task<bool> DoesPatientExist(int id)
@@ -38,3 +60,44 @@ public class DbContext: IDbContext
     {
         return await _context.Medicaments.AnyAsync(e => e.IdMedicament == id);
     }
+
+    public async Task CheckDate(AddPatientDTO addPatientDto)
+    {
+        if (!await DoesPatientExist(addPatientDto.Patient.IdPatient))
+        {
+            await AddPatient(addPatientDto.Patient);
+        }
+
+        foreach (var med in addPatientDto.Medicaments)
+        {
+            if (!await DoesMedicamentsExist(med.IdMedicament))
+            {
+                throw new Exception($"Medicament with Id {med.IdMedicament} does not exist");
+            }
+        }
+
+        var prescriptionEntity = new Prescription
+        {
+            IdPatient = addPatientDto.Patient.IdPatient,
+            Date = addPatientDto.Date,
+            DueDate = addPatientDto.DueDate ?? addPatientDto.Date
+        };
+
+        _context.Prescriptions.Add(prescriptionEntity);
+        await _context.SaveChangesAsync();
+
+        foreach (var med in addPatientDto.Medicaments)
+        {
+            var prescriptionMedicament = new Prescription_Medicament
+            {
+                IdPrescription = prescriptionEntity.IdPrescription,
+                IdMedicament = med.IdMedicament,
+                Dose = int.Parse(med.Dose),
+                Details = med.Description
+            };
+            _context.Prescription_Medicaments.Add(prescriptionMedicament);
+        }
+
+        await _context.SaveChangesAsync();
+    }
+}
